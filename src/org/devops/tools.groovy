7e8d82903b108e887cc85ea9bfa52ee3d37c8ecb
hellojack2019 <reblue520@163.com>
2021-03-08 16:46:45 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -2,6 +2,12 @@
 package org.devops
 
 //打印内容
-def PringMes(content){
-  println(content)
+def PringMes(value,color){
+  colors = ['red'  : "\033[40;31m >>>>>>${value}<<<<<<<< \033[0m",
+            'blue' : "\033[47;34m ${value} \033[0m",
+            'green' : "<x1b>[[1;32m >>>>>> ${value} >>>>>> <0x1b>[m",
+            'green1' : ""\033[40;32m >>>>>>${value} <<<<<<<< \033[0m"  ]
+   ansiColor('xterm'){
+        println(colors[color])
+   }
 }
