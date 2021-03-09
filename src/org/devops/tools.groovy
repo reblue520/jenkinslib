be8286aac23b6960329e7c0ab5993fd1c0a4da56
hellojack2019 <reblue520@163.com>
2021-03-09 20:27:27 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -6,7 +6,7 @@ def PringMes(value,color){
   colors = ['red'  : "\033[40;31m >>>>>>${value}<<<<<<<< \033[0m",
             'blue' : "\033[47;34m ${value} \033[0m",
             'green' : "<x1b>[[1;32m >>>>>> ${value} >>>>>> <0x1b>[m",
-            'green1' : ""\033[40;32m >>>>>>${value} <<<<<<<< \033[0m"  ]
+            'green1' : "\033[40;32m >>>>>>${value} <<<<<<<< \033[0m"  ]
    ansiColor('xterm'){
         println(colors[color])
    }
